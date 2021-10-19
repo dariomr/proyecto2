e47b4548c64413d26e20d0023aa744bb490df922
deinielos <dany_mg99@hotmail.com>
2021-10-19 12:49:46 +0200

    Cambios 3

--- data/animals.js
+++ data/animals.js
@@ -1,9 +1,9 @@
 import Animal from '../entities/Animal.js'
 
 const animals = [
-    new Animal ("Serpiente","carnivora",4,"Bamba Negra"),
+    new Animal ("Serpiente","carnivora",4,"Mamba Negra"),
     new Animal ("Tarantula","carnivora",1,"Africana"),
-    new Animal ("Oso","Omnivora",100,"Polar"),
+    new Animal ("Oso","Omnivora",100,"Pardo"),
 ]
 
 export{animals}
