910874121323dfa93b76ed26eae4b6554e8a5093
Darío <210188@fppiramide.com>
2021-10-19 12:52:34 +0200

    Added index.js

--- index.js
+++ index.js
@@ -9,9 +9,27 @@ import Animal from './entities/Animal.js'
 import {animals} from './data/animals.js'
 
 app.get('/',(req,res)=>{
-    res.render('index')
+    let arrayAnimales = []
+    let nombre = req.query.animal
+    console.log(nombre)
+    if(nombre) {
+        for (let i = 0; i < animals.length; I++) {
+            let animal = animals[i]
+            if (animal.nombre == nombre) {
+                arrayAnimales = [animal]
+            }
+        }
+    }
+    else {
+        arrayAnimales = [...animals]
+    }
+    res.render('index',{animals: arrayAnimales})
 })
 
 app.get('/gallery', (req,res)=>{
     res.render('gallery')
+})
+
+app.listen(8080,()=>{
+    console.log('server started')
 })
